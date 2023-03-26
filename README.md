edd15655e98f849e68070a7e41e223d78e34cea0
adisak <adisak@gmail.com>
2023-03-26 15:35:28 -0500

    slight mods
    minor modifications

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Hulu-Fade-Eliminator
 Hulu cosmetic changes: Remove Fade Scrims and Reduce amount of Fade over Controls / Subtitles
 
+Copyright 2023 Adisak Pochanayon
+contact: adisak@gmail.com
+
 Many thanks to Hide-Mouse-Hulu extension for the example of how to implement this
   https://github.com/NateXVI/Hide-Mouse-Hulu
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Hulu-Fade-Eliminator
+// Hulu cosmetic changes: Remove Fade Scrims and Reduce amount of Fade over Controls / Subtitles
+//
+// Copyright 2023 Adisak Pochanayon
+// contact: adisak@gmail.com
+//
+// Many thanks to Hide-Mouse-Hulu extension for the example of how to implement this
+// https://github.com/NateXVI/Hide-Mouse-Hulu
+
+
 // controls container variable declaration
 let controlsContainer = null;
 
