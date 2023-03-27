e83957bec4cbaba3c6855525f31f8d04d159734f
adisak <adisak@gmail.com>
2023-03-27 16:02:14 -0500

    Update
    - Incorporate back-in mouse hiding from "Hide-Mouse-Hulu"
    - Use IIFE to control scope

--- script.js
+++ script.js
@@ -7,12 +7,32 @@
 // Many thanks to Hide-Mouse-Hulu extension for the example of how to implement this
 // https://github.com/NateXVI/Hide-Mouse-Hulu
 
+//-----------------------------------------------------------------------------
+// Begin IIFE (Immediately Invoked Function Expression) Scope
+(() => {
+// Everything within here is "locally scoped"
+//-----------------------------------------------------------------------------
+
+// Beginning of "Hide-Mouse-Hulu" related code
 
 // controls container variable declaration
 let controlsContainer = null;
 
 // delay between searches for controls container (in milliseconds)
 const findDelay = 500;
+
+function setMouse(visibility) {
+	// sets the mouse visibility for the Hulu web player
+	try {
+		// set the web player element cursor style
+		document.querySelector('#web-player-app').style.cursor = visibility
+			? 'auto'
+			: 'none';
+	} catch (error) {
+		// if the mouse visibility cannot be set, log a warning in the console
+		console.warn('could not set mouse visibility', error);
+	}
+}
 
 function findControlsContainer() {
 	// look for a controls container
@@ -25,9 +45,39 @@ function findControlsContainer() {
 	}
 	// if the controls container can be found
 	else {
-		removeFadeBars();
+		// initialize the observer
+		initializeControlsObserver();
+
+		{
+		  // Call the code to remove the fade bars (not part of Hide-Mouse-Hulu)
+		  removeFadeBars();
+		}
 	}
 }
+
+function initializeControlsObserver() {
+	// creates an observer and sets it to look for changes in the controls container
+
+	function callback(mutations) {
+		// get the opacity of the controls container
+		let opacity = parseFloat(controlsContainer.style.opacity);
+
+		// make the mouse visible if the opacity is greater than 0.5
+		setMouse(opacity >= 0.5);
+	}
+
+	// create an observer
+	const observer = new MutationObserver(callback);
+
+	// set the observer to look for changes in the controls container
+	observer.observe(controlsContainer, {
+		attributes: true, //configure it to listen to attribute changes
+	});
+}
+
+// End of "Hide-Mouse-Hulu" related code
+
+//-----------------------------------------------------------------------------
 
 function removeCollection(collection)
 {
@@ -42,12 +92,12 @@ function nerfFlipTray()
 	// Using the "styles.css" file.  If anyone wants to figure this out, I'd love them forever.
 
 	// Make the fade area of the FliptrayWrapper less egregious
-	var flipWrapper = document.querySelector('.FliptrayWrapper');
+	let flipWrapper = document.querySelector('.FliptrayWrapper');
 	if (flipWrapper !== null) {
 		// This doesn't work?
 		flipWrapper.style.setProperty('background', 'linear-gradient(180deg, rgba(0,0,0,.0001), rgba(0,0,0,.0001) 60.0%, rgba(0,0,0,.2) 65%, rgba(0,0,0,.25) 70%);');
 
-		var flipStyle = getComputedStyle(flipWrapper, '::before');
+		let flipStyle = getComputedStyle(flipWrapper, '::before');
 		if (flipStyle !== null) {
 			// This gives an error because flipStyle is const!
 			flipStyle.background = 'linear-gradient(180deg, rgba(0,0,0,.0001), rgba(0,0,0,.0001) 60.0%, rgba(0,0,0,.2) 65%, rgba(0,0,0,.25) 70%);';
@@ -58,16 +108,30 @@ function nerfFlipTray()
 function removeFadeBars()
 {
 	// Remove the fade scrims at the top and bottom
-	var scrimTop = document.getElementsByClassName('ControlScrim__top');
+	let scrimTop = document.getElementsByClassName('ControlScrim__top');
 	if (scrimTop !== null) {
 		removeCollection(scrimTop);
 	}
-	var scrimBottom = document.getElementsByClassName('ControlScrim__bottom');
+	let scrimBottom = document.getElementsByClassName('ControlScrim__bottom');
 	if (scrimBottom !== null) {
 		removeCollection(scrimBottom);
 	}
 }
 
-// start finding the controls container
-findControlsContainer();
+function runExtension()
+{
+	// This is the "main" function for this Chrome Extension
+	
+	// start finding the controls container
+	findControlsContainer();
+}
+
+// Run the code for the Chrome Extension
+runExtension();
+
+//-----------------------------------------------------------------------------
+// End IIFE (Immediately Invoked Function Expression) Scope
+})();
+// Back to Global Scope
+//-----------------------------------------------------------------------------
 
